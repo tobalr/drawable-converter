a327ac66a241dfda4a9530cd3fc4c89fc4a0ff1b
tobalr <tobias.alroe@frameo.net>
2020-09-09 14:28:20 +0200

    Source image is now expected to be xxxhdpi
    This is the largest size used and thus it makes more source to use this
    as the source / original size.

--- converter.sh
+++ converter.sh
@@ -2,6 +2,7 @@ if [ $# -eq 0 ]; then
 	echo "No arguments supplied"
 else if [ -f "$1" ]; then
 	echo " Creating different dimensions (dips) of "$1" ..."
+	mkdir -p drawable-xxxhdpi
 	mkdir -p drawable-xxhdpi
 	mkdir -p drawable-xhdpi
 	mkdir -p drawable-hdpi
@@ -9,16 +10,18 @@ else if [ -f "$1" ]; then
 
 	if [ $1 = "ic_launcher.png" ]; then
 	    	echo "  App icon detected"
+		convert ic_launcher.png -resize 192x192 drawable-xxxhdpi/ic_launcher.png
 		convert ic_launcher.png -resize 144x144 drawable-xxhdpi/ic_launcher.png
 		convert ic_launcher.png -resize 96x96 drawable-xhdpi/ic_launcher.png
 		convert ic_launcher.png -resize 72x72 drawable-hdpi/ic_launcher.png
 		convert ic_launcher.png -resize 48x48 drawable-mdpi/ic_launcher.png
 		rm -i ic_launcher.png
 	else
-		convert $1 -resize 67% drawable-xhdpi/$1
-		convert $1 -resize 50% drawable-hdpi/$1
-		convert $1 -resize 33% drawable-mdpi/$1
-		mv $1 drawable-xxhdpi/$1
+		convert $1 -resize 75% drawable-xxhdpi/$1
+		convert $1 -resize 50% drawable-xhdpi/$1
+		convert $1 -resize 37.5% drawable-hdpi/$1
+    convert $1 -resize 25% drawable-mdpi/$1
+		mv $1 drawable-xxxhdpi/$1
 	fi
 	echo " Done"
 else
